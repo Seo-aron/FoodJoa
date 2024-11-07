49f0bcdeec29c46489ddbf882d3a938629c264f4
euneick <study.euneick@gmail.com>
2024-11-07 12:39:28 +0900

    코드 정리

--- src/VOs/MealkitOrderVO.java
+++ src/VOs/MealkitOrderVO.java
@@ -4,16 +4,17 @@ import java.sql.Date;
 
 public class MealkitOrderVO {
 	private int no;
-    private String id;
-    private int mealkitNo;
-    private int amount;
-    private String address;
-    private int delivered;
-    private int refund;
-    private Date postDate;
-    
-    // postDate없는 생성자  
-	public MealkitOrderVO(int no, String id, int mealkitNo, int amount, String address, int delivered, int refund) {
+	private String id;
+	private int mealkitNo;
+	private int amount;
+	private String address;
+	private int delivered;
+	private int refund;
+	private Date postDate;
+
+	// postDate없는 생성자
+	public MealkitOrderVO(int no, String id, int mealkitNo, int amount,
+			String address, int delivered, int refund) {
 		this.no = no;
 		this.id = id;
 		this.mealkitNo = mealkitNo;
@@ -23,15 +24,10 @@ public class MealkitOrderVO {
 		this.refund = refund;
 	}
 
-	public MealkitOrderVO(int no, String id, int mealkitNo, int amount, String address, int delivered, int refund,
-			Date postDate) {
-		this.no = no;
-		this.id = id;
-		this.mealkitNo = mealkitNo;
-		this.amount = amount;
-		this.address = address;
-		this.delivered = delivered;
-		this.refund = refund;
+	public MealkitOrderVO(int no, String id, int mealkitNo, int amount,
+			String address, int delivered, int refund, Date postDate) {
+
+		this(no, id, mealkitNo, amount, address, delivered, refund);
 		this.postDate = postDate;
 	}
 
@@ -98,5 +94,5 @@ public class MealkitOrderVO {
 	public void setPostDate(Date postDate) {
 		this.postDate = postDate;
 	}
-    
+
 }
--- src/VOs/MealkitVO.java
+++ src/VOs/MealkitVO.java
@@ -3,6 +3,7 @@ package VOs;
 import java.sql.Date;
 
 public class MealkitVO {
+	
 	private int no;
     private String id;
     private String title;
@@ -36,18 +37,8 @@ public class MealkitVO {
 
 	public MealkitVO(int no, String id, String title, String content, int category, String price, String pictures,
 			String orders, String origin, float rating, int views, int soldout, Date postDate) {
-		this.no = no;
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.category = category;
-		this.price = price;
-		this.pictures = pictures;
-		this.orders = orders;
-		this.origin = origin;
-		this.rating = rating;
-		this.views = views;
-		this.soldout = soldout;
+
+		this(no, id, title, content, category, price, pictures, orders, origin, rating, views, soldout);
 		this.postDate = postDate;
 	}
 
