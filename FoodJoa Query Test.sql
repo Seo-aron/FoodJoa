ae5513bb905489988fe50980f9e1f5a5b8d4ba1a
euneick <study.euneick@gmail.com>
2024-12-06 17:40:35 +0900

    파일 선택 이미지 변경

--- FoodJoa Query Test.sql
+++ FoodJoa Query Test.sql
@@ -1,11 +1,6 @@
-SELECT * FROM mealkit_cart;
-
-DELETE FROM mealkit_cart WHERE mealkit_no=1 OR mealkit_no=2;
-
-
-INSERT INTO mealkit_order (id, mealkit_no, address, quantity, delivered, refund, post_date) 
-VALUES ('3796760927', '1', '49122부산 영도구 태종로 69012213', 3, 0, 0, NOW()), 
-('3796760927', '2', '49122부산 영도구 태종로 69012213', 2, 0, 0, NOW()), 
-('3796760927', '3', '49122부산 영도구 태종로 69012213', 3, 0, 0, NOW()), 
-('3796760927', '4', '49122부산 영도구 태종로 69012213', 3, 0, 0, NOW()), 
-('3796760927', '5', '49122부산 영도구 태종로 69012213', 1, 0, 0, NOW());
+SELECT c.*, m.profile, m.nickname 
+FROM community_share c 
+LEFT OUTER JOIN member m 
+ON c.id = m.id 
+WHERE c.type like IF('재료나눔' like '%가%', 0, IF('같이먹어요' like '%가%', 1, -1))
+ORDER BY c.post_date DESC;
